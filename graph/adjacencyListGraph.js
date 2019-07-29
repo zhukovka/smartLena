dc9adbe7422105a71977f687b41ed2570693bbb9
zhukovka <helen.zhukova@gmail.com>
2019-07-29 08:16:26 +0300

    adjacency-list representation of a graph (start)

---
+++ graph/adjacencyListGraph.js
@@ -0,0 +1,10 @@
+/*
+adjacency-list representation of a graph G (V, E) consists of an array Adj of |V| lists,
+one for each vertex in V .
+For each u ∈ V , the adjacency list Adj[u] contains all the vertices v such that there is an edge (u, v) ∈ E.
+That is, Adj[u] consists of all the vertices adjacent to u in G
+ */
+
+function adjacencyListGraph () {
+
+}
